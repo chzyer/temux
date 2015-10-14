988a29f16000105dd5c9424048912a002d0f97a4
Cheney <me@chenye.org>
2015-10-14 20:49:40 +0800

    second commit

--- temux/temux.go
+++ temux/temux.go
@@ -5,23 +5,33 @@ import (
 	"os"
 	"os/exec"
 
+	"os/signal"
+	"syscall"
+
+	"github.com/chzyer/flagx"
 	"github.com/chzyer/temux/temux/term"
-	"github.com/pkg/term/termios"
 )
+
+type Config struct {
+	Shell string
+}
+
+func NewConfig() *Config {
+	var cfg Config
+	flagx.Parse(&cfg)
+	return &cfg
+}
 
 func main() {
 	cmd := exec.Command(os.Args[1], os.Args[2:]...)
-	pty, tty, err := termios.Pty()
+	pty, err := term.CopyTo(cmd, os.Stdin)
 	if err != nil {
 		panic(err)
 	}
-	cmd.Stdin = tty
-	cmd.Stdout = tty
-	cmd.Stderr = tty
+
 	if err := cmd.Start(); err != nil {
-		return
+		panic(err)
 	}
-	tty.Close()
 
 	t, err := term.New(os.Stdin, term.RawMode)
 	if err != nil {
@@ -31,5 +41,20 @@ func main() {
 
 	go io.Copy(os.Stdout, pty)
 	go io.Copy(pty, os.Stdin)
+
+	go func() {
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, syscall.SIGWINCH)
+		for {
+			select {
+			case <-ch:
+			}
+			if w, h, err := term.GetSize(os.Stdin.Fd()); err == nil {
+				pty.WindowChange(w, h)
+				cmd.Process.Signal(syscall.SIGWINCH)
+			}
+		}
+
+	}()
 	cmd.Wait()
 }
--- temux/term/term.go
+++ temux/term/term.go
@@ -2,8 +2,12 @@ package term
 
 import (
 	"os"
+	"os/exec"
+	"syscall"
+	"unsafe"
 
 	"github.com/pkg/term"
+	"github.com/pkg/term/termios"
 )
 
 type Term struct {
@@ -33,4 +37,101 @@ func (t *Term) SetOption(options ...func(*Term) error) error {
 		}
 	}
 	return nil
+}
+
+type Pty struct {
+	pty *os.File
+	tty *os.File
+}
+
+func NewPty() (*Pty, error) {
+	pty, tty, err := termios.Pty()
+	if err != nil {
+		return nil, err
+	}
+	return &Pty{
+		pty: pty,
+		tty: tty,
+	}, nil
+}
+
+func (p *Pty) Copy(o *os.File) (err error) {
+	var attr syscall.Termios
+	if err = termios.Tcgetattr(o.Fd(), &attr); err != nil {
+		return err
+	}
+	w, h, err := GetSize(o.Fd())
+	if err != nil {
+		return err
+	}
+
+	if err = SetSize(p.tty.Fd(), w, h); err != nil {
+		return err
+	}
+
+	err = termios.Tcsetattr(p.tty.Fd(), termios.TCSADRAIN, &attr)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *Pty) SetCmd(c *exec.Cmd) {
+	c.Stdin = p.tty
+	c.Stdout = p.tty
+	c.Stderr = p.tty
+	if c.SysProcAttr == nil {
+		c.SysProcAttr = &syscall.SysProcAttr{}
+	}
+	c.SysProcAttr.Setctty = true
+	c.SysProcAttr.Setsid = true
+}
+
+func (p *Pty) Read(b []byte) (int, error) {
+	return p.pty.Read(b)
+}
+
+func (p *Pty) Write(b []byte) (int, error) {
+	return p.pty.Write(b)
+}
+
+func (p *Pty) Close() {
+	p.tty.Close()
+	p.pty.Close()
+}
+
+func (p *Pty) WindowChange(w, h int) error {
+	return SetSize(p.pty.Fd(), w, h)
+}
+
+func GetSize(fd uintptr) (width, height int, err error) {
+	var dimensions [4]uint16
+	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL,
+		fd, syscall.TIOCGWINSZ, uintptr(unsafe.Pointer(&dimensions)))
+	if errno != 0 {
+		return -1, -1, errno
+	}
+	return int(dimensions[1]), int(dimensions[0]), nil
+}
+
+func SetSize(fd uintptr, width, height int) (err error) {
+	dimensions := [4]uint16{uint16(height), uint16(width), 0, 0}
+	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL,
+		fd, syscall.TIOCSWINSZ, uintptr(unsafe.Pointer(&dimensions)))
+	if errno != 0 {
+		return errno
+	}
+	return nil
+}
+
+func CopyTo(cmd *exec.Cmd, fd *os.File) (*Pty, error) {
+	pty, err := NewPty()
+	if err != nil {
+		return nil, err
+	}
+	if err := pty.Copy(os.Stdin); err != nil {
+		return nil, err
+	}
+	pty.SetCmd(cmd)
+	return pty, nil
 }
